643ab1e7162523ad6dd01c5bc9504696d788a0e1
Juan Orozco <juanthedesigner@gmail.com>
2014-08-13 00:38:06 -0400

    Add simple loading text.

--- js/main.js
+++ js/main.js
@@ -7,19 +7,27 @@ Handlebars.registerHelper('step', function (data) {
     return ret;
 });
 
-var appendSlides = function (data) {
+var appendSlides = function (data, cb) {
 
     var steps = data;
     var htmltemplate = $('#step-template').html();
     var htmltempl = Handlebars.compile(htmltemplate);
+    var total_steps = steps.length;
+    console.log("Steps to load: " + total_steps );
     steps.forEach(function (step, index) {
         var templ = htmltempl;
-        console.log(step);
+
         $.ajax({
             url: 'steps/' + step.uri,
             success: function (data) {
                 $('.steps').append(templ({file: data, data: step.data,
                                           class: step.class, id: step.id}));
+
+                if (total_steps <= index + 1)
+                {
+                  console.log("Done loading steps.");
+                  if (cb) cb();
+                }
             },
             async: false
         });
